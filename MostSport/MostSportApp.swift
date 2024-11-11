ccef0748c9302052dc88edd96abe6478915cdf6a
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-11-11 21:52:02 +0300

    Initial Commit

---
+++ MostSport/MostSportApp.swift
@@ -0,0 +1,17 @@
+//
+//  MostSportApp.swift
+//  MostSport
+//
+//  Created by Роман on 11.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MostSportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
